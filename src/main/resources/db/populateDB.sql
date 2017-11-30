dc3b50a0605852a2127d462425006c2e15854bfd
WladekK <kalinichenkovladimirv@gmail.com>
2017-11-30 16:51:04 +0200

    HW3 p.2 done: added script on drop meals and populate meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+delete from meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,7 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+insert into meals (user_id, date_time, description, calories) values
+  (100001, '19.12.1979', 'admin`s meal', 1000),
+  (100000, '05.10.1981', 'user`s meal', 1500);
